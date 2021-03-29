4c882bad48b8d82a0377638cb65eee7d4fa22fd1
jessemh <jessemh>
2021-03-29 11:04:12 -0400

    updates

--- quint.py
+++ quint.py
@@ -7,7 +7,7 @@ import os
 from os import path
 import argparse
 from pathlib import Path
-
+import platform
 
 parser = argparse.ArgumentParser()
 parser.add_argument('scant', nargs='?', type=str, help='help')
@@ -15,9 +15,12 @@ parser.add_argument('targ', nargs='?', type=str, help='help')
 args = parser.parse_args()
 scantype = args.scant
 target = args.targ
-print(target, scantype)
+print(scantype, target)
 
-home = str(Path.home()) + '/quint/logs'
+home = str(Path.home().joinpath('quint', 'logs'))
+nmapxmlpath = str(Path(home).joinpath('nmap_output.xml'))
+logfilepath = str(Path(home).joinpath('quint.json'))
+lastlogpath = str(Path(home).joinpath('lastlog.json'))
 def dnsQuery(rrtype, target):
     with urllib.request.urlopen("https://dns.google/resolve?name="+target+"&type="+rrtype) as url:
         data = json.loads(url.read().decode())
@@ -26,34 +29,37 @@ def nmapScan(params):
     match path.exists(home):
         case False:
             print("No Logs Directory, I'll create it...")
-            os.makedirs(home)
+            Path(home).mkdir(parents = True, exist_ok = True)
             nmapScan(params)
         case _:
             print("starting scan...")
-            os.system('sudo nmap -v0' + ' ' + params + '-oX ' + home + '/nmap_output.xml ' + target)
-            writeLogs()
+            match platform.system():
+                case 'Linux':
+                    os.system('sudo nmap -v0' + ' ' + params + '-oX ' + nmapxmlpath + ' ' + target)
+                    writeLogs()
+                case 'Windows':
+                    os.system('nmap -v0' + ' ' + params + '-oX ' + nmapxmlpath + ' ' + target)
+                    writeLogs()
 def writeLogs():
-    f = open(home + "/nmap_output.xml")
+    f = open(nmapxmlpath)
     xml_content = f.read()
     f.close()
-    log = open(home + "/quint.json", "a")
-    lastlog = open(home + "/lastlog.json", "w")
+    log = open(logfilepath, "a")
+    lastlog = open(lastlogpath, "w")
     newlog = [json.dumps(xmltodict.parse(xml_content), indent=4, sort_keys=True)]
     log.writelines(newlog)
     lastlog.writelines(newlog)
     log.close()
     lastlog.close()
 def readOutputLog():
-    with open(home + '/lastlog.json') as json_file:
-        outputjson = json.load(json_file)
-        clippedoutput = outputjson['nmaprun']['host']['hostscript']['script']['@output']
-        strclippedoutput = str(clippedoutput)
-        match strclippedoutput.find('>>>') != -1:
+    with open(lastlogpath) as json_file:
+        clippedoutput = str(json.load(json_file)['nmaprun']['host']['hostscript']['script']['@output'])
+        match clippedoutput.find('>>>') != -1:
             case True:
-                main, extra = strclippedoutput.split('>>>', 1)
+                main, extra = clippedoutput.split('>>>', 1)
                 print(main)
             case False:
-                print(strclippedoutput)
+                print(clippedoutput)
             case _:
                 print('Your output is messed up')
 
@@ -69,7 +75,7 @@ match scantype.upper():
         nmapscanoptions = '--script whois-ip --script-args whodb=nocache '
         nmapScan(nmapscanoptions)
         readOutputLog()
-    case "MX" | "A" | "SPF" | "ALL":
+    case "MX" | "A" | "SPF" | "ALL" | "CNAME":
         dnsQuery(scantype, target)
     case _:
         print("Nothing Selected")
